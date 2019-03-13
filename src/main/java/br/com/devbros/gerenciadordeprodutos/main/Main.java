3ee2c2b2a82e54bfaa7fb82180def6d3889e4266
ramse <ramse@DESKTOP-446LEIH>
2019-03-13 01:35:05 -0300

    Alterado a classe e pacote Testes para Main

---
+++ src/main/java/br/com/devbros/gerenciadordeprodutos/main/Main.java
@@ -0,0 +1,15 @@
+package br.com.devbros.gerenciadordeprodutos.main;
+
+import br.com.devbros.gerenciadordeprodutos.view.Menu;
+/**
+ * 
+ * @Ramses Souza
+ */
+public class Main 
+{
+     public static void main(String[] args) 
+    {      
+        Menu init = new Menu();
+        init.setVisible(true);
+    }
+}
