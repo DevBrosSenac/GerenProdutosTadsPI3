88fb1ad2bd99be91f74fa320d59546b89c1d79e5
Priscyla de Paula <sunsetpriscyla@gmail.com>
2019-03-13 22:13:26 -0300

    Edições de nomes das variáveis dos controles nas telas

--- src/main/java/br/com/devbros/gerenciadordeprodutos/view/ConsultaProduto.java
+++ src/main/java/br/com/devbros/gerenciadordeprodutos/view/ConsultaProduto.java
@@ -33,7 +33,7 @@ public class ConsultaProduto extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
-        pesquisaButton = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jSeparator1 = new javax.swing.JSeparator();
@@ -48,11 +48,11 @@ public class ConsultaProduto extends javax.swing.JFrame {
         vendaTxt = new javax.swing.JTextField();
         compraTxt = new javax.swing.JTextField();
         qtdSpinner = new javax.swing.JSpinner();
-        salvarButton = new javax.swing.JButton();
-        excluirButton = new javax.swing.JButton();
-        cancelarButton = new javax.swing.JButton();
-        editarButton = new javax.swing.JButton();
-        categoriaButton = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -60,7 +60,7 @@ public class ConsultaProduto extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(102, 102, 102));
         jLabel1.setText("CONSULTAR PRODUTOS");
 
-        pesquisaButton.setText("PESQUISAR");
+        jButton1.setText("PESQUISAR");
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -111,33 +111,33 @@ public class ConsultaProduto extends javax.swing.JFrame {
             }
         });
 
-        salvarButton.setText("SALVAR");
-        salvarButton.addActionListener(new java.awt.event.ActionListener() {
+        jButton2.setText("SALVAR");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                salvarButtonActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
 
-        excluirButton.setText("EXCLUIR");
+        jButton3.setText("EXCLUIR");
 
-        cancelarButton.setText("CANCELAR");
-        cancelarButton.addActionListener(new java.awt.event.ActionListener() {
+        jButton4.setText("CANCELAR");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cancelarButtonActionPerformed(evt);
+                jButton4ActionPerformed(evt);
             }
         });
 
-        editarButton.setText("EDITAR PRODUTO");
-        editarButton.addActionListener(new java.awt.event.ActionListener() {
+        jButton5.setText("EDITAR PRODUTO");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                editarButtonActionPerformed(evt);
+                jButton5ActionPerformed(evt);
             }
         });
 
-        categoriaButton.setText("SELECIONAR CATEGORIA...");
-        categoriaButton.addActionListener(new java.awt.event.ActionListener() {
+        jButton6.setText("SELECIONAR CATEGORIA...");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                categoriaButtonActionPerformed(evt);
+                jButton6ActionPerformed(evt);
             }
         });
 
@@ -152,11 +152,11 @@ public class ConsultaProduto extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jTextField1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(pesquisaButton))
+                        .addComponent(jButton1))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(editarButton)
+                        .addComponent(jButton5)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(categoriaButton))
+                        .addComponent(jButton6))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -180,11 +180,11 @@ public class ConsultaProduto extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(compraTxt, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(salvarButton)
+                        .addComponent(jButton2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(excluirButton)
+                        .addComponent(jButton3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cancelarButton))
+                        .addComponent(jButton4))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(qtdLabel)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -221,21 +221,21 @@ public class ConsultaProduto extends javax.swing.JFrame {
                             .addComponent(qtdSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(salvarButton)
-                            .addComponent(excluirButton)
-                            .addComponent(cancelarButton)))
+                            .addComponent(jButton2)
+                            .addComponent(jButton3)
+                            .addComponent(jButton4)))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(pesquisaButton))
+                            .addComponent(jButton1))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(editarButton)
-                            .addComponent(categoriaButton))
+                            .addComponent(jButton5)
+                            .addComponent(jButton6))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -259,7 +259,6 @@ public class ConsultaProduto extends javax.swing.JFrame {
         qtdLabel.setEnabled(false);
         qtdSpinner.setEnabled(false);
     }
-    
     public void ativarFormulario(){
         nomeLabel.setEnabled(true);
         nomeTxt.setEditable(true);
@@ -281,33 +280,33 @@ public class ConsultaProduto extends javax.swing.JFrame {
         qtdSpinner.setValue(0);
     }
     
-    private void cancelarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelarButtonActionPerformed
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
         Menu m = new Menu();
         m.setVisible(true);
         m.setResizable(false);
         m.setLocationRelativeTo(null);
         this.dispose();
-    }//GEN-LAST:event_cancelarButtonActionPerformed
+    }//GEN-LAST:event_jButton4ActionPerformed
 
-    private void categoriaButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_categoriaButtonActionPerformed
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
         ProdutoCategoria p = new ProdutoCategoria();
         p.setVisible(true);
         p.setResizable(false);
         p.setLocationRelativeTo(null);
         this.dispose();
-    }//GEN-LAST:event_categoriaButtonActionPerformed
+    }//GEN-LAST:event_jButton6ActionPerformed
 
-    private void editarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editarButtonActionPerformed
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
         ativarFormulario();
-    }//GEN-LAST:event_editarButtonActionPerformed
+    }//GEN-LAST:event_jButton5ActionPerformed
 
-    private void salvarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salvarButtonActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         limparFormulario();
-    }//GEN-LAST:event_salvarButtonActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -345,14 +344,16 @@ public class ConsultaProduto extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton cancelarButton;
-    private javax.swing.JButton categoriaButton;
     private javax.swing.JLabel compraLabel;
     private javax.swing.JTextField compraTxt;
     private javax.swing.JLabel descricaoLabel;
     private javax.swing.JTextField descricaoTxt;
-    private javax.swing.JButton editarButton;
-    private javax.swing.JButton excluirButton;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
@@ -361,10 +362,8 @@ public class ConsultaProduto extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel nomeLabel;
     private javax.swing.JTextField nomeTxt;
-    private javax.swing.JButton pesquisaButton;
     private javax.swing.JLabel qtdLabel;
     private javax.swing.JSpinner qtdSpinner;
-    private javax.swing.JButton salvarButton;
     private javax.swing.JLabel vendaLabel;
     private javax.swing.JTextField vendaTxt;
     // End of variables declaration//GEN-END:variables
